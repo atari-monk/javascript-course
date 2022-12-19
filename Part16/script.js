cb318e89af74726d0b3b8a0e3c24ae2955dd2c7c
krzm <kmazanek@gmail.com>
2022-12-19 15:38:39 +0100

    add pages

--- Part16/script.js
+++ Part16/script.js
@@ -8,6 +8,16 @@ samples["default-parameters"] = {
   script: "default-parameters.js",
   title: 'Part 16 - Default Parameters'
 }; 
+samples["value-ref"] = {
+  page: "sample.html",
+  script: "value-ref.js",
+  title: 'Part 16 - How Passing Arguments Works: Values vs. Reference'
+}; 
+samples["func-as-arg"] = {
+  page: "sample.html",
+  script: "func-as-arg.js",
+  title: 'Part 16 - Functions accepting callback functions'
+}; 
 
 window.samples = samples;
 window.onClick = onClick;

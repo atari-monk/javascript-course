a76167c943ec578f30307bc37bde6e014edf9ea5
krzm <kmazanek@gmail.com>
2022-12-21 19:17:05 +0100

    Refactor index

--- Part16-17/script.js
+++ Part16-17/script.js
@@ -32,7 +32,8 @@ samples["bind-method"] = {
   page: "sample.html",
   script: "bind-method.js",
   title: 'Part 16 - The bind Method'
-}; 
+};
+
 samples["challenge-1"] = {
   page: "sample.html",
   script: "challenge-1.js",
@@ -47,21 +48,45 @@ samples["closures"] = {
   page: "sample.html",
   script: "closures.js",
   title: 'Vid 17 - Closures'
-}; 
+};
+
+//vid18
 samples["arrays"] = {
   page: "sample.html",
   script: "arrays.js",
   title: 'Vid 18 - Arrays'
-}; 
+};
 samples["arrays-foreach"] = {
   page: "sample2.html",
   script: "arrays-foreach.js",
   title: 'Vid 18 - Arrays ForEach'
-}; 
+};
 samples["map-set-foreach"] = {
   page: "sample2.html",
   script: "map-set-foreach.js",
-  title: 'Vid 18 - Map, Set ForEach'
+  title: 'Map, Set ForEach',
+  vid: '18',
+  start: '37',
+  end: '42',
+  time: '5'
+}; 
+samples["bankist-app-intro"] = {
+  page: "../Bankist/index.html",
+  script: "../Bankist/script.js",
+  title: 'Bankist App Intro',
+  vid: '18',
+  start: '43',
+  end: '53',
+  time: '10'
+}; 
+samples["bankist-app-1"] = {
+  page: "../Bankist/index.html",
+  script: "../Bankist/script.js",
+  title: 'Bankist App 1',
+  vid: '18',
+  start: '53',
+  end: '1:11',
+  time: '18'
 }; 
 samples["v18-challenge-1"] = {
   page: "sample2.html",
@@ -78,31 +103,97 @@ samples["filter-method"] = {
   script: "filter-method.js",
   title: 'Vid 18, Filter Method'
 };
-samples["reduce-method"] = {
+
+//vid19
+const vid19 = []; 
+vid19["reduce-method"] = {
   page: "sample2.html",
   script: "reduce-method.js",
-  title: 'Vid 19, Reduce Method'
+  title: 'Reduce Method',
+  vid: '19',
+  start: '0',
+  end: '10',
+  time: '10'
 };
-samples["v19-challenge-1"] = {
+vid19["bankist-2"] = {
+  page: "../Bankist/index.html",
+  script: "../Bankist/script.js",
+  title: 'Bankist App 2',
+  vid: '19',
+  start: '10',
+  end: '14',
+  time: '10'
+};
+vid19["reduce-method-2"] = {
+  page: "sample2.html",
+  script: "reduce-method.js",
+  title: 'Reduce Method, Max',
+  vid: '19',
+  start: '14',
+  end: '21',
+  time: '7'
+};
+vid19["s2-challenge-2"] = {
   page: "sample2.html",
   script: "v19-challenge-1.js",
-  title: 'Vid 19, Challenge 1'
+  title: 'Challenge #2',
+  vid: '19',
+  start: '21',
+  end: '31',
+  time: '10'
 };
-samples["chaining-methods"] = {
+vid19["chaining-methods"] = {
   page: "sample2.html",
   script: "chaining-methods.js",
-  title: 'Vid 19, Chaining Methods'
+  title: 'Chaining Methods',
+  vid: '19',
+  start: '31',
+  end: '31',
+  time: '0'
 };
-samples["v19-challenge-2"] = {
+vid19["v19-challenge-2"] = {
   page: "sample2.html",
   script: "v19-challenge-2.js",
   title: 'Vid 19, Challenge 2'
 };
-samples["find-method"] = {
+vid19["find-method"] = {
   page: "sample2.html",
   script: "find-method.js",
   title: 'Vid 19, Find Method'
 };
 
 window.samples = samples;
+window.vid19 = vid19;
 window.onClick = onClick;
+
+const getById = function (id) {
+  return document.getElementById(id);
+}
+
+const setTitle = function (element, data) {
+  element.innerText = `${data.title} (${data.start}-${data.end}/${data.time})`; 
+}
+
+const vid18Keys = [
+  'map-set-foreach'
+  ,'bankist-app-intro'
+  , 'bankist-app-1'
+];
+
+vid18Keys.forEach((key)=>{
+  setTitle(getById(key), samples[key]);
+});
+
+const vid19Keys = [
+  'reduce-method',
+  'bankist-2',
+  'reduce-method-2',
+  's2-challenge-2',
+  'chaining-methods'
+];
+
+console.log(Object.keys(vid19));
+
+vid19Keys.forEach((key) => {
+  setTitle(getById(key), vid19[key]);
+});

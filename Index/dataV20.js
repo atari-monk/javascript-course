72a6bfb9c9ac47364b45eb0dfef7dab804e5c5c1
krzm <kmazanek@gmail.com>
2022-12-22 16:21:59 +0100

    setup vid 20

---
+++ Index/dataV20.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const vid20 = [];
+vid20["bankist-4"] = {
+  page: "../Bankist/index.html",
+  script: "../Bankist/script.js",
+  title: 'Bankist App 4',
+  vid: '20',
+  start: '0',
+  end: 'x',
+  time: 'x'
+};
+vid20["x"] = {
+  page: "./Index/sample2.html",
+  script: "../V20/x.js",
+  title: '',
+  vid: '20',
+  start: 'x',
+  end: 'x',
+  time: 'x'
+};
+
+export { vid20 }
--- Index/script.js
+++ Index/script.js
@@ -5,11 +5,13 @@ import { vid16 } from './dataV16.js';
 import { vid17 } from './dataV17.js';
 import { vid18 } from './dataV18.js';
 import { vid19 } from './dataV19.js';
+import { vid20 } from './dataV20.js';
 
 window.vid16 = vid16;
 window.vid17 = vid17;
 window.vid18 = vid18;
 window.vid19 = vid19;
+window.vid20 = vid20;
 window.onClick = onClick;
 
 const getById = function (id) {
@@ -31,4 +33,7 @@ Object.keys(vid18).forEach((key) => {
 });
 Object.keys(vid19).forEach((key) => {
   setTitle(getById(key), vid19[key]);
+});
+Object.keys(vid20).forEach((key) => {
+  setTitle(getById(key), vid20[key]);
 });

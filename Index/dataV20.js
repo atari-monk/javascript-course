75aded8a886c3c136b8ca31b0d9ca9155c36a237
krzm <kmazanek@gmail.com>
2022-12-23 16:00:02 +0100

    Add flat page

--- Index/dataV20.js
+++ Index/dataV20.js
@@ -28,14 +28,40 @@ vid20["bankist-6"] = {
   end: '58',
   time: '13'
 };
-vid20["some-every"] = {
+vid20["some-every-1"] = {
   page: "./Index/sample2.html",
   script: "../V20/some-every.js",
   title: 'Some and Every Method.',
   vid: '20',
   start: '58',
+  end: '1:02',
+  time: '4'
+};
+vid20["bankist-7"] = {
+  page: "../Bankist/index.html",
+  script: "../Bankist/script.js",
+  title: 'Bankist App 7, Loan, Some Method.',
+  vid: '20',
+  start: '1:02',
+  end: '1:09',
+  time: '7'
+};
+vid20["some-every-2"] = {
+  page: "./Index/sample2.html",
+  script: "../V20/some-every.js",
+  title: 'Some and Every Method.',
+  vid: '20',
+  start: '1:09',
+  end: '1:13',
+  time: '4'
+};
+vid20["flat"] = {
+  page: "./Index/sample2.html",
+  script: "../V20/flat.js",
+  title: 'Flat & FlatMap Method.',
+  vid: '20',
+  start: '1:13',
   end: 'x',
   time: 'x'
 };
-
 export { vid20 }

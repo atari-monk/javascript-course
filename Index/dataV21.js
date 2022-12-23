702c88bf1b1147eb08291cc12be006724c60de93
krzm <kmazanek@gmail.com>
2022-12-23 20:06:38 +0100

    Add V21 placeholder

---
+++ Index/dataV21.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const vid21 = [];
+vid21["x"] = {
+  page: "./Index/sample2.html",
+  script: "../V21/x.js",
+  title: '.',
+  vid: '21',
+  start: 'x',
+  end: 'x',
+  time: 'x'
+};
+
+export { vid21 }
--- Index/script.js
+++ Index/script.js
@@ -6,12 +6,14 @@ import { vid17 } from './dataV17.js';
 import { vid18 } from './dataV18.js';
 import { vid19 } from './dataV19.js';
 import { vid20 } from './dataV20.js';
+import { vid21 } from './dataV21.js';
 
 window.vid16 = vid16;
 window.vid17 = vid17;
 window.vid18 = vid18;
 window.vid19 = vid19;
 window.vid20 = vid20;
+window.vid21 = vid21;
 window.onClick = onClick;
 
 const getById = function (id) {
@@ -36,4 +38,7 @@ Object.keys(vid19).forEach((key) => {
 });
 Object.keys(vid20).forEach((key) => {
   setTitle(getById(key), vid20[key]);
+});
+Object.keys(vid21).forEach((key) => {
+  setTitle(getById(key), vid21[key]);
 });

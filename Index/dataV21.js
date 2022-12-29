7f506cc6541a054acf786167ce505d576e6fb737
atari-monk <atari.monk1@gmail.com>
2022-12-29 20:10:21 +0100

    add s2-challenge-4

--- Index/dataV21.js
+++ Index/dataV21.js
@@ -11,4 +11,14 @@ vid21["array-methods"] = {
   time: '32'
 };
 
+vid21["s2-challenge-4"] = {
+  page: "./Index/sample2.html",
+  script: "../V21/s2-challenge-4.js",
+  title: 'Challenge #4',
+  vid: '21',
+  start: '39',
+  end: 'x',
+  time: 'x'
+};
+
 export { vid21 }

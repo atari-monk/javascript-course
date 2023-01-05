b846142945726703b1e99773085619a1f49c85b0
atari-monk <atari.monk1@gmail.com>
2023-01-05 18:00:39 +0100

    add 6 sections

--- Index/dataV24.js
+++ Index/dataV24.js
@@ -52,4 +52,64 @@ vid24["oop3"] = {
   time: '15'
 };
 
+vid24["oop-ex1"] = {
+  page: "./Index/sample2.html",
+  script: "../V24/oop-ex1.js",
+  title: 'Coding Challenge #1',
+  vid: '24',
+  start: '1:27',
+  end: '1:34',
+  time: '7'
+};
+
+vid24["oop4"] = {
+  page: "./Index/sample2.html",
+  script: "../V24/oop4.js",
+  title: 'ES6 Classes',
+  vid: '24',
+  start: '1:34',
+  end: '1:47',
+  time: '13'
+};
+
+vid24["oop5"] = {
+  page: "./Index/sample2.html",
+  script: "../V24/oop5.js",
+  title: 'Setters and Getters (also added in ES6 Classes)',
+  vid: '24',
+  start: '1:47',
+  end: '2:0',
+  time: '13'
+};
+
+vid24["oop6"] = {
+  page: "./Index/sample2.html",
+  script: "../V24/oop4.js",
+  title: 'Static Methods',
+  vid: '24',
+  start: '2:0',
+  end: '2:7',
+  time: '7'
+};
+
+vid24["oop7"] = {
+  page: "./Index/sample2.html",
+  script: "../V24/oop6.js",
+  title: 'Object.create',
+  vid: '24',
+  start: '2:7',
+  end: '2:18',
+  time: '11'
+};
+
+vid24["oop-ex2"] = {
+  page: "./Index/sample2.html",
+  script: "../V24/oop-ex2.js",
+  title: 'Coding Challenge #2',
+  vid: '24',
+  start: '2:18',
+  end: '2:24',
+  time: '6'
+};
+
 export { vid24 }
---
+++ V24/oop4.js
@@ -0,0 +1,63 @@
+'use strict'
+
+// ES6 Classes
+
+// Class expression
+// const Person = class {}
+
+// Class declaration
+class Person {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+
+  // Instance methods
+  // Methods will be added to .prototype property
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+
+  greet() {
+    console.log(`Hey ${this.fullName}`);
+  }
+
+  get age() {
+    return 2037 - this.birthYear;
+  }
+
+  // Set a property that already exists
+  set fullName(name) {
+    if (name.includes(' ')) this._fullName = name;
+    else alert(`${name} is not a full name!`);
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
+
+  // Static method
+  static hey() {
+    console.log('Hey there 👋');
+    console.log(this);
+  }
+}
+
+const jessica = new Person('Jessica Davis', 1996);
+console.log(jessica);
+jessica.calcAge();
+console.log(jessica.age);
+
+console.log(jessica.__proto__ === Person.prototype);
+
+// Person.prototype.greet = function () {
+//   console.log(`Hey ${this.firstName}`);
+// };
+jessica.greet();
+
+// 1. Classes are NOT hoisted
+// 2. Classes are first-class citizens
+// 3. Classes are executed in strict mode
+
+const walter = new Person('Walter White', 1965);
+Person.hey();

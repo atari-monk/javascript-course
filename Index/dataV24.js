17ecd7b4dc0166dd66296c136a8d5186e7fb27df
atari-monk <atari.monk1@gmail.com>
2023-01-05 11:22:02 +0100

    set vid24

---
+++ Index/dataV24.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const vid24 = [];
+
+vid24["oop-theory1"] = {
+  page: "./index.html",
+  script: "./Index/script.js",
+  title: 'OOP Theory',
+  vid: '24',
+  start: '0',
+  end: 'x',
+  time: 'x'
+};
+
+export { vid24 }

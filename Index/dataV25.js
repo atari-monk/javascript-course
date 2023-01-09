d3f2e4fa22b78a3edf07e1209d6a6f8e64879eef
atari-monk <atari.monk1@gmail.com>
2023-01-09 12:17:00 +0100

    add place holder

--- Index/dataV25.js
+++ Index/dataV25.js
@@ -5,9 +5,19 @@ const vid25 = [];
 vid25["geo1"] = {
   page: "../App-Map-Page/index.html",
   script: "../App-Map-Page/script.js",
-  title: 'App Map Page',
+  title: 'App Map Page, Intro',
   vid: '25',
   start: '0',
+  end: '6',
+  time: '6'
+};
+
+vid25["geo2"] = {
+  page: "../App-Map-Page/index.html",
+  script: "../App-Map-Page/script.js",
+  title: 'App Map Page, Theory, How To Plan A Web Project',
+  vid: '25',
+  start: '6',
   end: 'x',
   time: 'x'
 };

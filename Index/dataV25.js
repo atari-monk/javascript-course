35fcbd126f70322b813dfd5fd536e51675a97a0f
atari-monk <atari.monk1@gmail.com>
2023-01-09 17:30:07 +0100

    Add flowchart

--- Index/dataV25.js
+++ Index/dataV25.js
@@ -18,6 +18,16 @@ vid25["geo2"] = {
   title: 'App Map Page, Theory, How To Plan A Web Project',
   vid: '25',
   start: '6',
+  end: '24',
+  time: '24'
+};
+
+vid25["geo3"] = {
+  page: "../App-Map-Page/index.html",
+  script: "../App-Map-Page/script.js",
+  title: 'App Map Page, Code, Using the Geolocation API',
+  vid: '25',
+  start: '24',
   end: 'x',
   time: 'x'
 };

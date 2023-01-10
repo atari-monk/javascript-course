638f7b889c362d579d76d6fdfe18b6a55affb1ca
atari-monk <atari.monk1@gmail.com>
2023-01-10 13:00:16 +0100

    add 3 sections

--- Index/dataV25.js
+++ Index/dataV25.js
@@ -28,6 +28,36 @@ vid25["geo3"] = {
   title: 'App Map Page, Code, Using the Geolocation API',
   vid: '25',
   start: '24',
+  end: '32',
+  time: '8'
+};
+
+vid25["geo4"] = {
+  page: "../App-Map-Page/index.html",
+  script: "../App-Map-Page/script.js",
+  title: 'App Map Page, Code, Displaying a map using leaflet library',
+  vid: '25',
+  start: '32',
+  end: '46',
+  time: '14'
+};
+
+vid25["geo5"] = {
+  page: "../App-Map-Page/index.html",
+  script: "../App-Map-Page/script.js",
+  title: 'App Map Page, Code, Displaying a map marker',
+  vid: '25',
+  start: '46',
+  end: '1:06',
+  time: '20'
+};
+
+vid25["geo6"] = {
+  page: "../App-Map-Page/index.html",
+  script: "../App-Map-Page/script.js",
+  title: 'App Map Page, Code, Rendering workout input form',
+  vid: '25',
+  start: '1:06',
   end: 'x',
   time: 'x'
 };

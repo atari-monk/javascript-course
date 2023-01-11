ec12b9f9c11076bb7f18605c438d8f535062b90d
atari-monk <atari.monk1@gmail.com>
2023-01-11 13:23:31 +0100

    Refactor app to module

---
+++ App-Map-Page/Cycling.js
@@ -0,0 +1,21 @@
+'use strict';
+
+import { Workout } from "./Workout.js";
+
+export class Cycling extends Workout {
+  type = 'cycling';
+
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    // this.type = 'cycling';
+    this.calcSpeed();
+    this._setDescription();
+  }
+
+  calcSpeed() {
+    // km/h
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}

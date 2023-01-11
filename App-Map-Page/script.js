25dd2161760503e67e87b18c6186d95d1fba766f
atari-monk <atari.monk1@gmail.com>
2023-01-11 13:13:36 +0100

    Finish vid 25

--- App-Map-Page/script.js
+++ App-Map-Page/script.js
@@ -13,7 +13,6 @@ class Workout {
 
   _setDescription() {
     const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-
     this.description = `${this.type[0].toUpperCase()}${this.type.slice(1)} on ${months[this.date.getMonth()]
       } ${this.date.getDate()}`;
   }
@@ -72,13 +71,16 @@ const inputElevation = document.querySelector('.form__input--elevation');
 
 class App {
   #map;
+  #mapZoomLevel = 13;
   #mapEvent;
   #workouts = [];
 
   constructor() {
     this.#getPosition();
+    this.#getLocalStorage();
     form.addEventListener('submit', this.#newWorkout.bind(this));
     inputType.addEventListener('change', this.#toggleElevationField);
+    containerWorkouts.addEventListener('click', this.#moveToPopup.bind(this));
   }
 
   #getPosition() {
@@ -104,12 +106,23 @@ class App {
     }).addTo(this.#map);
 
     this.#map.on('click', this.#showForm.bind(this));
+
+    this.#workouts.forEach(work => {
+      this.#renderWorkoutMarker(work);
+    });
   }
 
   #showForm(mapE) {
     this.#mapEvent = mapE;
     form.classList.remove('hidden');
     inputDistance.focus();
+  }
+
+  #hideForm() {
+    inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = '';
+    form.style.display = 'none';
+    form.classList.add('hidden');
+    setTimeout(() => (form.style.display = 'grid'), 1000);
   }
 
   #toggleElevationField() {
@@ -143,13 +156,13 @@ class App {
       workout = new Cycling([lat, lng], distance, duration, elevation);
     }
     this.#workouts.push(workout);
-    this._renderWorkoutMarker(workout);
-
-    inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = '';
-
+    this.#renderWorkoutMarker(workout);
+    this.#renderWorkout(workout);
+    this.#hideForm();
+    this.#setLocalStorage();
   }
 
-  _renderWorkoutMarker(workout) {
+  #renderWorkoutMarker(workout) {
     L.marker(workout.coords)
       .addTo(this.#map)
       .bindPopup(
@@ -163,6 +176,100 @@ class App {
       .setPopupContent(
         `${workout.type === 'running' ? '🏃‍♂️' : '🚴‍♀️'} ${workout.description}`)
       .openPopup();
+  }
+
+  #renderWorkout(workout) {
+    let html = `
+      <li class="workout workout--${workout.type}" data-id="${workout.id}">
+        <h2 class="workout__title">${workout.description}</h2>
+        <div class="workout__details">
+          <span class="workout__icon">${workout.type === 'running' ? '🏃‍♂️' : '🚴‍♀️'
+      }</span>
+          <span class="workout__value">${workout.distance}</span>
+          <span class="workout__unit">km</span>
+        </div>
+        <div class="workout__details">
+          <span class="workout__icon">⏱</span>
+          <span class="workout__value">${workout.duration}</span>
+          <span class="workout__unit">min</span>
+        </div>
+    `;
+
+    if (workout.type === 'running')
+      html += `
+        <div class="workout__details">
+          <span class="workout__icon">⚡️</span>
+          <span class="workout__value">${workout.pace.toFixed(1)}</span>
+          <span class="workout__unit">min/km</span>
+        </div>
+        <div class="workout__details">
+          <span class="workout__icon">🦶🏼</span>
+          <span class="workout__value">${workout.cadence}</span>
+          <span class="workout__unit">spm</span>
+        </div>
+      </li>
+      `;
+
+    if (workout.type === 'cycling')
+      html += `
+        <div class="workout__details">
+          <span class="workout__icon">⚡️</span>
+          <span class="workout__value">${workout.speed.toFixed(1)}</span>
+          <span class="workout__unit">km/h</span>
+        </div>
+        <div class="workout__details">
+          <span class="workout__icon">⛰</span>
+          <span class="workout__value">${workout.elevationGain}</span>
+          <span class="workout__unit">m</span>
+        </div>
+      </li>
+      `;
+
+    form.insertAdjacentHTML('afterend', html);
+  }
+
+  #moveToPopup(e) {
+    // BUGFIX: When we click on a workout before the map has loaded, we get an error. But there is an easy fix:
+    if (!this.#map) return;
+
+    const workoutEl = e.target.closest('.workout');
+
+    if (!workoutEl) return;
+
+    const workout = this.#workouts.find(
+      work => work.id === workoutEl.dataset.id
+    );
+
+    this.#map.setView(workout.coords, this.#mapZoomLevel, {
+      animate: true,
+      pan: {
+        duration: 1,
+      },
+    });
+
+    // using the public interface
+    //workout.click();
+  }
+
+  #setLocalStorage() {
+    localStorage.setItem('workouts', JSON.stringify(this.#workouts));
+  }
+
+  #getLocalStorage() {
+    const data = JSON.parse(localStorage.getItem('workouts'));
+
+    if (!data) return;
+
+    this.#workouts = data;
+
+    this.#workouts.forEach(work => {
+      this.#renderWorkout(work);
+    });
+  }
+
+  reset() {
+    localStorage.removeItem('workouts');
+    location.reload();
   }
 }
 
--- Index/dataV25.js
+++ Index/dataV25.js
@@ -5,7 +5,7 @@ const vid25 = [];
 vid25["geo1"] = {
   page: "../App-Map-Page/index.html",
   script: "../App-Map-Page/script.js",
-  title: 'App Map Page, Intro',
+  title: 'Intro',
   vid: '25',
   start: '0',
   end: '6',
@@ -15,7 +15,7 @@ vid25["geo1"] = {
 vid25["geo2"] = {
   page: "../App-Map-Page/index.html",
   script: "../App-Map-Page/script.js",
-  title: 'App Map Page, Theory, How To Plan A Web Project',
+  title: 'Theory, How To Plan A Web Project',
   vid: '25',
   start: '6',
   end: '24',
@@ -25,7 +25,7 @@ vid25["geo2"] = {
 vid25["geo3"] = {
   page: "../App-Map-Page/index.html",
   script: "../App-Map-Page/script.js",
-  title: 'App Map Page, Code, Using the Geolocation API',
+  title: 'Using the Geolocation API',
   vid: '25',
   start: '24',
   end: '32',
@@ -35,7 +35,7 @@ vid25["geo3"] = {
 vid25["geo4"] = {
   page: "../App-Map-Page/index.html",
   script: "../App-Map-Page/script.js",
-  title: 'App Map Page, Code, Displaying a map using leaflet library',
+  title: 'Displaying a map using leaflet library',
   vid: '25',
   start: '32',
   end: '46',
@@ -45,7 +45,7 @@ vid25["geo4"] = {
 vid25["geo5"] = {
   page: "../App-Map-Page/index.html",
   script: "../App-Map-Page/script.js",
-  title: 'App Map Page, Code, Displaying a map marker',
+  title: 'Displaying a map marker',
   vid: '25',
   start: '46',
   end: '1:06',
@@ -55,7 +55,7 @@ vid25["geo5"] = {
 vid25["geo6"] = {
   page: "../App-Map-Page/index.html",
   script: "../App-Map-Page/script.js",
-  title: 'App Map Page, Code, Rendering workout input form',
+  title: 'Rendering workout input form',
   vid: '25',
   start: '1:06',
   end: '1:22',
@@ -65,7 +65,7 @@ vid25["geo6"] = {
 vid25["geo7"] = {
   page: "../App-Map-Page/index.html",
   script: "../App-Map-Page/script.js",
-  title: 'App Map Page, Theory, Project Architecture',
+  title: 'Theory, Project Architecture',
   vid: '25',
   start: '1:22',
   end: '1:32',
@@ -75,7 +75,7 @@ vid25["geo7"] = {
 vid25["geo8"] = {
   page: "../App-Map-Page/index.html",
   script: "../App-Map-Page/script.js",
-  title: 'App Map Page, Code, Refactoring for project Architecture',
+  title: 'Refactoring for project Architecture',
   vid: '25',
   start: '1:32',
   end: '1:56',
@@ -85,7 +85,7 @@ vid25["geo8"] = {
 vid25["geo9"] = {
   page: "../App-Map-Page/index.html",
   script: "../App-Map-Page/script.js",
-  title: 'App Map Page, Code, Creating Classes for workout data',
+  title: 'Creating Classes for workout data',
   vid: '25',
   start: '1:56',
   end: '2:12',
@@ -95,7 +95,7 @@ vid25["geo9"] = {
 vid25["geo10"] = {
   page: "../App-Map-Page/index.html",
   script: "../App-Map-Page/script.js",
-  title: 'App Map Page, Code, Creating new workout',
+  title: 'Creating new workout',
   vid: '25',
   start: '2:12',
   end: '2:46',
@@ -105,11 +105,41 @@ vid25["geo10"] = {
 vid25["geo11"] = {
   page: "../App-Map-Page/index.html",
   script: "../App-Map-Page/script.js",
-  title: 'App Map Page, Code, Rendering workouts',
+  title: 'Rendering workouts',
   vid: '25',
   start: '2:46',
-  end: 'x',
-  time: 'x'
+  end: '3:11',
+  time: '25'
+};
+
+vid25["geo12"] = {
+  page: "../App-Map-Page/index.html",
+  script: "../App-Map-Page/script.js",
+  title: 'Move to marker on click',
+  vid: '25',
+  start: '3:11',
+  end: '3:27',
+  time: '16'
+};
+
+vid25["geo13"] = {
+  page: "../App-Map-Page/index.html",
+  script: "../App-Map-Page/script.js",
+  title: 'Working with localstorage',
+  vid: '25',
+  start: '3:27',
+  end: '3:53',
+  time: '26'
+};
+ 
+vid25["geo14"] = {
+  page: "../App-Map-Page/index.html",
+  script: "../App-Map-Page/script.js",
+  title: 'Theory, Final considerations',
+  vid: '25',
+  start: '3:53',
+  end: '3:58',
+  time: '5'
 };
 
 export { vid25 }
